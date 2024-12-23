bb6b86073c78aa42b672b23d0671119f5c24a527
Harsh Gondaliya <157871191+HarshGondaliya78@users.noreply.github.com>
2024-12-23 20:04:36 +0530

    initial commit

--- mux_8X1/mux_8to1.v
+++ mux_8X1/mux_8to1.v
@@ -6,14 +6,14 @@ module mux_8to1(
    );
   always @(*)begin
     case(s) 
-      3'b000 : y=i[0];
-      3'b001 : y=i[1];
-      3'b010 : y=i[2];
-      3'b011 : y=i[3];
-      3'b100 : y=i[4];
-      3'b101 : y=i[5];
-      3'b110 : y=i[6];
-      3'b111 : y=i[7];
+      3'b000 : y<=i[0];
+      3'b001 : y<=i[1];
+      3'b010 : y<=i[2];
+      3'b011 : y<=i[3];
+      3'b100 : y<=i[4];
+      3'b101 : y<=i[5];
+      3'b110 : y<=i[6];
+      3'b111 : y<=i[7];
       default:$display("Invalid input");
     endcase
   end
