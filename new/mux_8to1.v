5605b7dbd80ea13e81ac10c29621d7cd9d1769a9
Harsh Gondaliya <157871191+HarshGondaliya78@users.noreply.github.com>
2024-12-23 15:56:06 +0530

    initial commit

--- new/mux_8to1.v
+++ new/mux_8to1.v
@@ -1,11 +1,13 @@
 `timescale 1ns / 1ps
+// 8-to-1 Multiplexer: Selects one of 8 inputs based on a 3-bit select signal.
 module mux_8to1(
-   input [7:0] i,
-   input [2:0] s,
-   output reg  y
+   input [7:0] i,  // i: 8-bit input lines for the multiplexer.
+   input [2:0] s,  // s: 3-bit select signal for choosing the input line.
+   output reg  y   // y: Output signal corresponding to the selected input.
    );
-  always @(*)begin
-    case(s) 
+ 
+   always @(*)begin
+    case(s)        // Case statement to select one of the 8 input lines based on the select signal 's'.
       3'b000 : y=i[0];
       3'b001 : y=i[1];
       3'b010 : y=i[2];
@@ -14,7 +16,7 @@ module mux_8to1(
       3'b101 : y=i[5];
       3'b110 : y=i[6];
       3'b111 : y=i[7];
-      default:$display("Invalid input");
-    endcase
+    default:$display("Invalid input");  // Handles invalid select signal values and displays an error message.
+   endcase
   end
 endmodule
