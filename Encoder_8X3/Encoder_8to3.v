132f3343bc2f16deec5a04e89e8a8cf4cfd32845
Harsh Gondaliya <157871191+HarshGondaliya78@users.noreply.github.com>
2024-12-24 14:50:44 +0530

    Update Encoder_8to3.v

--- Encoder_8X3/Encoder_8to3.v
+++ Encoder_8X3/Encoder_8to3.v
@@ -1,20 +1,21 @@
 `timescale 1ns / 1ps
-module Encoder_8to3(
-    input [7:0] i,
-    output reg [2:0] y
-    );
-    
-   always @(*) begin
-    case(i)
-      8'b00000001:y<=3'b000;
-      8'b00000010:y<=3'b001;
-      8'b00000100:y<=3'b010;
-      8'b00001000:y<=3'b011;
-      8'b00010000:y<=3'b100;
-      8'b00100000:y<=3'b101;
-      8'b01000000:y<=3'b110;
-      8'b10000000:y<=3'b111;
-      default:y=3'bxxx;
-    endcase
-   end 
+module Encoder_8to3_tb();
+    reg [7:0] i; // 8-bit register to drive the input of the encoder
+    wire [2:0] y; // 3-bit wire to observe the output of the encoder
+
+    Encoder_8to3 uut(.i(i),.y(y)); //connect testbench input or output to the module's input or output
+    initial begin
+        // Apply different test cases to the input and wait for 10 time units between each
+        i=8'b00000001;#10;
+        i=8'b00000010;#10;
+        i=8'b00000100;#10;
+        i=8'b00001000;#10;
+        i=8'b00010000;#10;
+        i=8'b00100000;#10;
+        i=8'b01000000;#10;
+        i=8'b10000000;#10;
+        i=8'b00000000;#10;
+        $finish; // End the simulation
+    end
+
 endmodule
