8ccb451d7e2b2cf0a3d1bed4127f1b69aeea3cb9
Harsh Gondaliya <157871191+HarshGondaliya78@users.noreply.github.com>
2024-12-24 13:51:23 +0530

    initial commit

--- Decoder_3X8/Decoder_3to8_tb.v
+++ Decoder_3X8/Decoder_3to8_tb.v
@@ -4,24 +4,11 @@ module Decoder_3to8_tb();
   wire [7:0] out;
   integer temp;
   
-  Decoder_3to8 a(.in(in),.out(out));
-  
-  initial begin
-  
+Decoder_3to8 a(.in(in),.out(out));  
+initial begin
     for (temp=0;temp<8;temp=temp+1) begin
      in=temp;#10;
-     
-    end
-//  initial
-//  begin
-//   in=3'b000;#10;
-//   in=3'b001;#10;
-//   in=3'b010;#10;
-//   in=3'b011;#10;
-//   in=3'b100;#10;
-//   in=3'b101;#10;
-//   in=3'b110;#10;
-//   in=3'b111;#10;  
+    end  
   $finish;
  end
 endmodule
